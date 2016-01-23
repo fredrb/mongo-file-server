e8f9ba01cc5296b923c3af808dbb24db4151c50c
Frederico <fred.rbittencourt@gmail.com>
2016-01-23 14:51:33 -0200

    use localhost config

--- app/index.js
+++ app/index.js
@@ -58,29 +58,7 @@ module.exports = (function() {
         _connectDB() {
             mongoose.connect("mongodb://" + dbConfig.user + ":" + dbConfig.password + "@" + dbConfig.host + "/" + dbConfig.db);
         }
-
-
     }
 
     return Server;
 });
-
-// module.exports = function(server, port, user, password, db) {
-//     var app = express();
-//     var fileSchema = require('./schema/file');
-//
-//     app.use(bodyParser.json());
-//
-//     mongoose.connect("mongodb://" + user + ":" + password + "@" + server + "/" + db);
-//
-//     app.get('/', function(req, res) {
-//         fileSchema.find({}, {'_id' : 0, 'name' : 1}, function(err, data) {
-//             if(err) res.send(err.message);
-//             res.json(data);
-//         });
-//     });
-//
-//     app.listen("3000", function() {
-//         console.log("Applications started on port 3000");
-//     });
-// };
