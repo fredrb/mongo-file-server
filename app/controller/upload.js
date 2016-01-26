5c4bd067e2fe1e3f35e1ee738732d79afea43da6
Frederico <fred.rbittencourt@gmail.com>
2016-01-26 07:21:20 -0200

    mongo field validations

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -9,6 +9,7 @@ module.exports = (function() {
   let upload       = multer({dest : uploadConfig.dest});
 
   function _format(data, template) {
+    if (!data) return null;
     let responseObj = {};
     template.forEach(t => {
       responseObj[t] = data[t];
@@ -27,22 +28,27 @@ module.exports = (function() {
   });
 
   router.post('/upload', upload.single('file'), (req, res, next) => {
+    if (req.file == null)
+      res.send({
+        'success' : false,
+        'message' : 'No document to be uploaded \'file\' parameter'
+      });
     fs.readFile(req.file.path, (err, data) => {
       let _file = new FileModel({
-        name : req.body.name,
+        name : req.body.name || null,
         bin : data
       });
 
       _file.save((err, data) => {
         if (err) {
-          res.send({
-            'sucess' : false,
-            'message' : err.Message
+          return res.send({
+            'success' : false,
+            'message' : err.message
           });
         }
-        
+
         res.send({
-          'sucess' : true,
+          'success' : true,
           'message' : 'Object saved',
           'data' : _format(data, ['_id', 'name'])
         });
--- app/schema/file.js
+++ app/schema/file.js
@@ -1,8 +1,15 @@
+'use strict';
+
 var mongoose = require('mongoose');
+function notNullValidator(value, respond) {
+  respond((value != null) && (value.length > 0), 'Invalid length');
+}
 
 var file = new mongoose.Schema({
   name : String,
   bin : Buffer
 });
+
+file.path('name').validate(notNullValidator);
 
 module.exports = mongoose.model('file', file);
